57625827685f92fb80bf7c8f409b53ba1b88c308
Jonathan Hansen <kriah0872@gmail.com>
2020-04-21 14:02:22 -0700

    layout for project page and home completed

---
+++ js/app.js
@@ -0,0 +1,20 @@
+$(document).foundation()
+
+    VANTA.NET({
+    el: ".main",
+    mouseControls: false,
+    touchControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0x161616,
+    backgroundColor: 0xffffff,
+    maxDistance: 26.00,
+    spacing: 17.00
+  })
+
+
+
+
+
