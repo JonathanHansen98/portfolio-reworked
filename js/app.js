111e43a5571ca5f072f007db64cde0d66399cfd9
Jonathan Hansen <kriah0872@gmail.com>
2020-04-24 03:16:28 -0700

    animation work

--- js/app.js
+++ js/app.js
@@ -1,20 +1,42 @@
 $(document).foundation()
 
-    VANTA.NET({
-    el: ".main",
-    mouseControls: false,
-    touchControls: false,
-    minHeight: 200.00,
-    minWidth: 200.00,
-    scale: 1.00,
-    scaleMobile: 1.00,
-    color: 0x161616,
-    backgroundColor: 0xffffff,
-    maxDistance: 26.00,
-    spacing: 17.00
-  })
+let animation = () => {
+    var nameAn = TweenMax.to('#name', 1, {
+        scaleY: "1",
+        ease: "expo"
+    })
+    let titleAn = TweenMax.to('#title', 1, {
+        scaleY: "1",
+        ease: "expo.out"
+    }).delay(.5)
+    let lineAn = TweenMax.to('.line', 1, {
+        scaleX: "1",
+        ease: "expo.out"
+    }).delay(.5)
+    let logoAn = TweenMax.to('#devlogo', 1, {
+        opacity: "1",
+        ease: "expo.out"
+    }).delay(.7)
 
+    let resetClicked = false;
 
+    $("#reset").click(() => {
+        if (resetClicked === false) {
+            nameAn.reverse()
+            titleAn.reverse()
+            lineAn.reverse()
+            logoAn.reverse()
+            resetClicked = true
+            $("#reset").text("View Logo")
+            $("#logo-an").hide()
+            $("#about").show()
+        }
+        else {
+            animation()
+            resetClicked = false
+        } 
 
+    })
+}
 
-
+animation()
